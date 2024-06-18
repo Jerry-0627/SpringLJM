bbe1bcc856f4f56ba4d3c3d8c1674fe2fb35acf7
Jerry-0627 <mincand0627@gmail.com>
2024-06-18 15:43:40 +0900

    .

--- src/main/java/com/jerry/LJM/student/StudentController.java
+++ src/main/java/com/jerry/LJM/student/StudentController.java
@@ -1,7 +1,9 @@
 package com.jerry.LJM.student;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StudentController {
 
 	@RequestMapping("/student/list")
