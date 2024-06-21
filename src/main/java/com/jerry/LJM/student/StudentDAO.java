bd2a96822c0a1c6879c73a816cf3299f4bef5eeb
Jerry-0627 <mincand0627@gmail.com>
2024-06-22 00:00:27 +0900

    6.22

--- src/main/java/com/jerry/LJM/student/StudentDAO.java
+++ src/main/java/com/jerry/LJM/student/StudentDAO.java
@@ -1,5 +1,6 @@
 package com.jerry.LJM.student;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
@@ -8,7 +9,9 @@ public class StudentDAO {
 	public void getList() throws Exception {
 		File file = new File("c:\\study\\student.txt");
 		FileReader fr = new FileReader(file);
-
+		BufferedReader br = new BufferedReader(fr);
+		
+		String str1 = br.readLine();
 	}
 
 }
