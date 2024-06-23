1b6514e3e756d811b7a628e8abe0f279f0b8d3cf
Jerry-0627 <mincand0627@gmail.com>
2024-06-24 00:02:17 +0900

    asd

--- src/main/java/com/jerry/LJM/student/StudentDAO.java
+++ src/main/java/com/jerry/LJM/student/StudentDAO.java
@@ -2,16 +2,33 @@ package com.jerry.LJM.student;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class StudentDAO {
+	
+	private File file;
+	
+	public StudentDAO() {
+		this.file = new File("C:\\study\\student.txt");		
+	}
 
 	public void getList() throws Exception {
-		File file = new File("c:\\study\\student.txt");
-		FileReader fr = new FileReader(file);
-		BufferedReader br = new BufferedReader(fr);
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		String info = br.readLine();
+		String[] infos = null;
+		List<StudentDTO> sDTOs = new ArrayList<StudentDTO>();
 		
-		String str1 = br.readLine();
+		while(info != null) {
+			infos = info.split(",");
+			if(info.length() < 5) break;
+		}
 	}
 
 }
